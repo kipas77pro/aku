1ebbb04429457b06f21d6a2dd40f0b6f12c68832
kipas77pro <lahbaru930@gmail.com>
2025-07-07 11:29:57 +0700

    Add files via upload

--- setup3.sh
+++ setup3.sh
@@ -386,10 +386,6 @@ fi
 
 curl -sS ifconfig.me > /etc/myipvps
 
-## fix Dropbear ##
-chmod 755 /usr/sbin/dropbear
-systemctl restart dropbear
-
 #install gotop
 gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
     gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
