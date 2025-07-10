59aeba0bef5eeb8cb83ea309d5ca6a0138c602af
kipas77pro <lahbaru930@gmail.com>
2025-07-10 07:30:43 +0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -397,7 +397,16 @@ gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | 
     gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
     curl -sL "$gotop_link" -o /tmp/gotop.deb
     dpkg -i /tmp/gotop.deb >/dev/null 2>&1
-    
+
+# memory swap 1gb
+cd
+dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+mkswap /swapfile
+chown root:root /swapfile
+chmod 0600 /swapfile >/dev/null 2>&1
+swapon /swapfile >/dev/null 2>&1
+sed -i '$ i\/swapfile      swap swap   defaults    0 0' /etc/fstab
+
 clear
 echo  ""
 echo  "Sukses Sayank..!!"
