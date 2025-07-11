d59c84b3631f2b7919a7db96cafef3245ddd2d90
kipas77pro <lahbaru930@gmail.com>
2025-07-11 11:14:55 +0700

    Delete a.sh

--- a.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install squid
-cd
-apt -y install squid3
-wget -O /etc/squid/squid.conf "raw.githubusercontent.com/Arya-Blitar22/vps/main/ssh/squid3.conf"
-sed -i $MYIP2 /etc/squid/squid.conf
