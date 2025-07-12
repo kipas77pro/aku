556a8b74aa1a9e190721ad247a94988ec9d43ab2
kipas77pro <lahbaru930@gmail.com>
2025-07-12 09:07:43 +0700

    Add files via upload

--- opok/menu.sh
+++ opok/menu.sh
@@ -46,7 +46,7 @@ else
 resst="${red}OFF${NC}"
 fi
 # // SSH Websocket Proxy
-ssh_ws=$( systemctl status ws | grep Active | awk '{print $3}' | sed 's/(//g' | sed 's/)//g' )
+ssh_ws=$(ssh_ws=$(systemctl status ws | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
 if [[ $ssh_ws == "running" ]]; then
     status_ws_epro="${green}ON${NC}"
 else
@@ -58,7 +58,7 @@ resngx="${green}ON${NC}"
 else
 resngx="${red}OFF${NC}"
 fi
-dbr=$(service dropbear status | grep active | cut -d ' ' $stat)
+dbr=$(etc/init.d/dropbear status | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
 if [ "$dbr" = "active" ]; then
 resdbr="${green}ON${NC}"
 else
