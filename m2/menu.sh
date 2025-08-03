0aa228fddcd790fad1c091c144b9ac6883170e10
kipas77pro <lahbaru930@gmail.com>
2025-08-04 05:39:37 +0700

    Update menu.sh

--- m2/menu.sh
+++ m2/menu.sh
@@ -151,7 +151,7 @@ echo
 read -p " Select menu : " opt
 echo -e ""
 case $opt in
-1) clear ; m-ssh ;;
+1) clear ; menu-ssh ;;
 2) clear ; menu-vmess ;;
 3) clear ; menu-vless ;;
 4) clear ; menu-trojan ;;
