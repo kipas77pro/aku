a16886da5897f028e422afa112c0ebe7eee3f874
kipas77pro <lahbaru930@gmail.com>
2025-08-03 21:44:25 +0700

    Update ws.py

--- websocket_engine/ws.py
+++ websocket_engine/ws.py
@@ -17,8 +17,8 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:109'
-RESPONSE = (b'HTTP/1.1 101 <font color="green"><b>WA ARYA BLITAR 081931615811 Akunssh Pusat Blitar<br> </i></b></font>\r\n'
+DEFAULT_HOST = '127.0.0.1:22'
+RESPONSE = (b'HTTP/1.1 101 <font color="green"><b>WA ARYA BLITAR 081931615811<br> </i></b></font>\r\n'
             b'Upgrade: websocket\r\n'
             b'Connection: Upgrade\r\n'
             b'Sec-WebSocket-Accept: foo\r\n\r\n')
