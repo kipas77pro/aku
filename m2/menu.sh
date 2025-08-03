e9c824d2c292be1a286baf0a71f5f06aef4b1562
kipas77pro <lahbaru930@gmail.com>
2025-08-04 06:28:27 +0700

    Add files via upload

--- m2/menu.sh
+++ m2/menu.sh
@@ -114,7 +114,7 @@ echo -e "${BICyan} │                    ${NC}ALLXRAY ${ICyan}: ${ORANGE}$vma  
 echo -e " ${BICyan}╰═════════════════════════════════════════════════════╯${NC}"
 echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
 echo -e "           ${NC} SSH ${ORANGE}: ${RED}$ressh"" ${NC} XRAY ${ORANGE}: ${RED}$resv2r"" ${NC} TROJAN ${ORANGE}: ${RED}$resv2r"
-echo -e "      ${NC} NGINX ${ORANGE}: ${RED}$resngx "" ${NC}DROPBEAR ${ORANGE}: ${RED}$resdbr" "${NC} SSH-WS ${ORANGE}: ${RED}$status_ws_epro"
+echo -e "        ${NC} NGINX ${ORANGE}: ${RED}$resngx "" ${NC}DROPBEAR ${ORANGE}: ${RED}$resdbr" "${NC} SSH-WS ${ORANGE}: ${RED}$status_ws_epro"
 echo -e " ${BICyan}└─────────────────────────────────────────────────────┘${NC}"
 echo -e "${BICyan} ┌─────────────────────────────────────────────────────┐${NC}"
 echo -e "      ${NC} Hari ini                    Bulan ini  ${NC}"
