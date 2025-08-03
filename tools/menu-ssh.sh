db2388b1763e53cb8103a9259429b0d8ee911725
kipas77pro <lahbaru930@gmail.com>
2025-08-04 05:35:37 +0700

    Add files via upload

---
+++ tools/menu-ssh.sh
@@ -0,0 +1,352 @@
+#!/bin/bash
+NC='\033[0;37m' 
+PURPLE='\033[0;34m' 
+GREEN='\033[0;32m' 
+RED='\033[0;31m'
+BIWhite='\033[1;97m'  
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+clear
+function del(){
+clear
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m               MEMBER SSH                 \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"      
+echo "USERNAME          EXP DATE          STATUS"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+while read expired
+do
+AKUN="$(echo $expired | cut -d: -f1)"
+ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+status="$(passwd -S $AKUN | awk '{print $2}' )"
+if [[ $ID -ge 1000 ]]; then
+if [[ "$status" = "L" ]]; then
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED${NORMAL}"
+else
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED${NORMAL}"
+fi
+fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo ""
+echo -e "\E[42;1;37m                DELETE USER               \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"  
+echo ""
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna > /dev/null 2>&1
+        echo -e "User $Pengguna was removed."
+else
+        echo -e "Failure: User $Pengguna Not Exist."
+fi
+
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu-ssh
+}
+function autodel(){
+clear
+               hariini=`date +%d-%m-%Y`
+               echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+               echo -e "\E[42;1;37m                AUTO DELETE               \E[0m"
+               echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"  
+               echo "Thank you for removing the EXPIRED USERS"
+               echo -e "$PURPLE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"  
+               cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+               totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+               for((i=1; i<=$totalaccounts; i++ ))
+               do
+               tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+               username=`echo $tuserval | cut -f1 -d:`
+               userexp=`echo $tuserval | cut -f2 -d:`
+               userexpireinseconds=$(( $userexp * 86400 ))
+               tglexp=`date -d @$userexpireinseconds`             
+               tgl=`echo $tglexp |awk -F" " '{print $3}'`
+               while [ ${#tgl} -lt 2 ]
+               do
+               tgl="0"$tgl
+               done
+               while [ ${#username} -lt 15 ]
+               do
+               username=$username" " 
+               done
+               bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+               echo "echo "Expired- User : $username Expire at : $tgl $bulantahun"" >> /usr/local/bin/alluser
+               todaystime=`date +%s`
+               if [ $userexpireinseconds -ge $todaystime ] ;
+               then
+		    	:
+               else
+               echo "echo "Expired- Username : $username are expired at: $tgl $bulantahun and removed : $hariini "" >> /usr/local/bin/deleteduser
+	           echo "Username $username that are expired at $tgl $bulantahun removed from the VPS $hariini"
+               userdel $username
+               fi
+               done
+               echo " "
+               echo -e "$PURPLE━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  
+               
+               read -n 1 -s -r -p "Press any key to back on menu"
+               menu-ssh
+        
+}
+function ceklim(){
+clear
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m          CEK USER MULTILOGIN      \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+if [ -e "/root/log-limit.txt" ]; then
+echo "User Who Violate The Maximum Limit";
+echo "Time - Username - Number of Multilogin"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+cat /root/log-limit.txt
+else
+echo " No user has committed a violation"
+echo " "
+echo " or"
+echo " "
+echo " The user-limit script not been executed."
+fi
+echo " ";
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo " ";
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+}
+
+function member(){
+clear
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m               MEMBER SSH                 \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"      
+echo "USERNAME          EXP DATE          STATUS"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+while read expired
+do
+AKUN="$(echo $expired | cut -d: -f1)"
+ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+status="$(passwd -S $AKUN | awk '{print $2}' )"
+if [[ $ID -ge 1000 ]]; then
+if [[ "$status" = "L" ]]; then
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED${NORMAL}"
+else
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED${NORMAL}"
+fi
+fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo "Account number: $JUMLAH user"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-ssh
+}
+function renew(){
+clear
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m               MEMBER SSH                 \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"      
+echo "USERNAME          EXP DATE          STATUS"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+while read expired
+do
+AKUN="$(echo $expired | cut -d: -f1)"
+ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+status="$(passwd -S $AKUN | awk '{print $2}' )"
+if [[ $ID -ge 1000 ]]; then
+if [[ "$status" = "L" ]]; then
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED${NORMAL}"
+else
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED${NORMAL}"
+fi
+fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo -e ""
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m            Perpanjang  User              \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"  
+echo
+read -p "Username : " User
+egrep "^$User" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+read -p "Day Extend : " Days
+Today=`date +%s`
+Days_Detailed=$(( $Days * 86400 ))
+Expire_On=$(($Today + $Days_Detailed))
+Expiration=$(date -u --date="1970-01-01 $Expire_On sec GMT" +%Y/%m/%d)
+Expiration_Display=$(date -u --date="1970-01-01 $Expire_On sec GMT" '+%d %b %Y')
+passwd -u $User
+usermod -e  $Expiration $User
+egrep "^$User" /etc/passwd >/dev/null
+echo -e "$Pass\n$Pass\n"|passwd $User &> /dev/null
+clear
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m            Perpanjang  User              \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"  
+echo -e ""
+echo -e " Username : $User"
+echo -e " Days Added : $Days Days"
+echo -e " Expires on :  $Expiration_Display"
+echo -e ""
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+else
+clear
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m            Perpanjang  User              \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"  
+echo -e ""
+echo -e "   Username Doesnt Exist      "
+echo -e ""
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+fi
+read -n 1 -s -r -p "Press any key to back on menu"
+menu-ssh
+}
+function autokill(){
+clear
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[ON]${Font_color_suffix}"
+Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
+cek=$(grep -c -E "^# Autokill" /etc/cron.d/tendang)
+if [[ "$cek" = "1" ]]; then
+sts="${Info}"
+else
+sts="${Error}"
+fi
+clear
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "\E[42;1;37m             AUTOKILL SSH          \E[0m"
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "Status Autokill : $sts        "
+echo -e ""
+echo -e "[1]  AutoKill After 5 Minutes"
+echo -e "[2]  AutoKill After 10 Minutes"
+echo -e "[3]  AutoKill After 15 Minutes"
+echo -e "[4]  Turn Off AutoKill/MultiLogin"
+echo -e "[x]  Menu"
+echo ""
+echo -e "${PURPLE}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e ""
+read -p "Select From Options [1-4 or x] :  " AutoKill
+read -p "Multilogin Maximum Number Of Allowed: " max
+echo -e ""
+case $AutoKill in
+                1)
+                echo -e ""
+                sleep 1
+                clear
+                echo > /etc/cron.d/tendang
+                echo "# Autokill" >/etc/cron.d/tendang
+                echo "*/5 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang && chmod +x /etc/cron.d/tendang
+                echo "" > /root/log-limit.txt
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "      Allowed MultiLogin : $max"
+                echo -e "      AutoKill Every     : 5 Minutes"      
+                echo -e ""
+                echo -e "======================================"
+                service cron reload >/dev/null 2>&1
+                service cron restart >/dev/null 2>&1                                                                 
+                ;;
+                2)
+                echo -e ""
+                sleep 1
+                clear
+                echo > /etc/cron.d/tendang
+                echo "# Autokill" >/etc/cron.d/tendang
+                echo "*/10 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang && chmod +x /etc/cron.d/tendang
+                echo "" > /root/log-limit.txt
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "      Allowed MultiLogin : $max"
+                echo -e "      AutoKill Every     : 10 Minutes"
+                echo -e ""
+                echo -e "======================================"
+                service cron reload >/dev/null 2>&1
+                service cron restart >/dev/null 2>&1
+                ;;
+                3)
+                echo -e ""
+                sleep 1
+                clear
+                echo > /etc/cron.d/tendang
+                echo "# Autokill" >/etc/cron.d/tendang
+                echo "*/15 * * * *  root /usr/bin/tendang $max" >>/etc/cron.d/tendang && chmod +x /etc/cron.d/tendang
+                echo "" > /root/log-limit.txt
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "      Allowed MultiLogin : $max"
+                echo -e "      AutoKill Every     : 15 Minutes"
+                echo -e ""
+                echo -e "======================================"
+                service cron reload >/dev/null 2>&1
+                service cron restart >/dev/null 2>&1          
+                ;;
+                4)
+                rm -fr /etc/cron.d/tendang
+                echo "" > /root/log-limit.txt
+                echo -e ""
+                echo -e "======================================"
+                echo -e ""
+                echo -e "      AutoKill MultiLogin Turned Off"
+                echo -e ""
+                echo -e "======================================"
+                service cron reload >/dev/null 2>&1
+                service cron restart >/dev/null 2>&1
+                ;;
+                x)
+                menu
+                ;;
+                *)
+                echo "Please enter an correct number"
+                ;;
+        esac
+read -n 1 -s -r -p "Press any key to back on menu"
+m-ssh
+}
+clear
+echo -e "${PURPLE}┌─────────────────────────────────────────────────┐${NC}"
+echo -e "${PURPLE} \E[42;1;37m                     SSH MENU                    ${PURPLE}│$NC"
+echo -e "${PURPLE}└─────────────────────────────────────────────────┘${NC}"
+echo -e "${PURPLE}┌───────────────────────────────────────────────┐${NC}"
+echo -e "     ${GREEN}[1]${NC}  Create Ssh Account "
+echo -e "     ${GREEN}[2]${NC}  Trial Ssh Acoount  "
+echo -e "     ${GREEN}[3]${NC}  Delete Ssh Acoount  "
+echo -e "     ${GREEN}[4]${NC}  Perpanjang Ssh Account  "
+echo -e "     ${GREEN}[5]${NC}  Cek User Login "
+echo -e "     ${GREEN}[6]${NC}  Cek User Multi Log "
+echo -e "     ${GREEN}[7]${NC}  Auto Del User Exp  "
+echo -e "     ${GREEN}[8]${NC}  Auto Kill User Ssh "
+echo -e "     ${GREEN}[9]${NC}  Cek All Member Ssh "
+echo -e "     ${GREEN}[10]${NC} Tendang User Multi"
+echo -e "     ${GREEN}[11]${NC} Cek User Udp"
+echo -e " "
+echo -e "     ${GREEN}[0]${NC} Back To Menu      "
+echo -e "${PURPLE}└───────────────────────────────────────────────┘${NC}"
+echo ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; usernew ;;
+2) clear ; trialssh ;;
+3) clear ; del ;;
+4) clear ; renew;;
+5) clear ; cekssh ;;
+6) clear ; ceklim ;;
+7) clear ; autodel ;;
+8) clear ; autokill ;;
+9) clear ; member ;;
+10) clear ; tendang ;;
+11) clear ; cekudp ;;
+0) clear ; menu ;;
+*) echo -e "" ; echo "Press any key to back on menu" ; sleep 1 ; menu ;;
+esac
