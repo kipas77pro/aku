8960c87171a55e13c2c14c54dc27c67bca05407b
kipas77pro <lahbaru930@gmail.com>
2025-08-04 06:56:31 +0700

    Add files via upload

--- ssh/ssh-vpn4.sh
+++ ssh/ssh-vpn4.sh
@@ -223,8 +223,41 @@ apt-get -y --purge remove apache2* >/dev/null 2>&1
 apt-get -y --purge remove bind9* >/dev/null 2>&1
 apt-get -y remove sendmail* >/dev/null 2>&1
 apt autoremove -y >/dev/null 2>&1
-
+# install stunnel
 clear
+
+cd /root/
+apt install stunnel4 -y
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+
+[dropbear]
+accept = 447
+connect = 127.0.0.1:109
+[openssh]
+accept = 444
+connect = 127.0.0.1:22
+[openssh]
+accept = 777
+connect = 127.0.0.1:2253
+[openssh]
+accept = 445
+connect = 127.0.0.1:40000
+END
+
+# make a certificate
+openssl genrsa -out key.pem 2048
+openssl req -new -x509 -key key.pem -out cert.pem -days 1095 \
+-subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email"
+cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
+
+# konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+/etc/init.d/stunnel4 restart >/dev/null 2>&1
 
 # Install bbr
 sleep 1
@@ -308,6 +341,27 @@ apt -y install fail2ban
 sudo systemctl enable --now fail2ban
 /etc/init.d/fail2ban restart
 /etc/init.d/fail2ban status
+# Instal DDOS Flate
+rm -fr /usr/local/ddos
+mkdir -p /usr/local/ddos >/dev/null 2>&1
+#clear
+sleep 1
+echo -e "[ ${green}INFO$NC ] Install DOS-Deflate"
+sleep 1
+echo -e "[ ${green}INFO$NC ] Downloading source files..."
+wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
+wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
+wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
+wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
+chmod 0755 /usr/local/ddos/ddos.sh
+cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos  >/dev/null 2>&1
+sleep 1
+echo -e "[ ${green}INFO$NC ] Create cron script every minute...."
+/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
+sleep 1
+echo -e "[ ${green}INFO$NC ] Install successfully..."
+sleep 1
+echo -e "[ ${green}INFO$NC ] Config file at /usr/local/ddos/ddos.conf"
 
 # Banner /etc/issue.net
 rm -fr /etc/issue.net
